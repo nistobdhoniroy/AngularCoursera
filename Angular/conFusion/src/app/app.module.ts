030fca2180d0df4d8348b5e78599f910434a547c
nistobdhoniroy <bsse0813@iit.du.ac.bd>
2020-04-29 19:53:08 +0600

    basic service

--- Angular/conFusion/src/app/app.module.ts
+++ Angular/conFusion/src/app/app.module.ts
@@ -17,7 +17,7 @@ import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
-
+import {DishService} from './services/dish.service';
 
 @NgModule({
   declarations: [
@@ -36,7 +36,9 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
     MatCardModule,
     MatButtonModule
   ],
-  providers: [],
+  providers: [
+    DishService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
