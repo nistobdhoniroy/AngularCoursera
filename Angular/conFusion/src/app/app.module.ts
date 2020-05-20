e5ddcdf73155b9bdc159a435117bd1c56ab132be
nistobdhoniroy <bsse0813@iit.du.ac.bd>
2020-05-20 21:35:50 +0600

    Dialog

--- Angular/conFusion/src/app/app.module.ts
+++ Angular/conFusion/src/app/app.module.ts
@@ -11,6 +11,13 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms'; 
+
+
 import { AppComponent } from './app.component';
 
 import 'hammerjs';
@@ -27,6 +34,7 @@ import {PromotionService} from './services/promotion.service';
 import {LeaderService} from './services/leader.service';
 
 import {AppRoutingModule} from './app-routing/app-routing.module';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
@@ -37,7 +45,8 @@ import {AppRoutingModule} from './app-routing/app-routing.module';
     FooterComponent,
     HomeComponent,
     AboutComponent,
-    ContactComponent
+    ContactComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -49,7 +58,15 @@ import {AppRoutingModule} from './app-routing/app-routing.module';
     MatGridListModule,
     MatCardModule,
     MatButtonModule,
-    AppRoutingModule
+    MatFormFieldModule, 
+    MatInputModule,
+    MatCheckboxModule,
+    MatDialogModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  entryComponents:[
+    LoginComponent
   ],
   providers: [
     DishService,
