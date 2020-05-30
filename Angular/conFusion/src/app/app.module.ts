4947f00290f56577596624f69990d4280ad601c5
nistobdhoniroy <bsse0813@iit.du.ac.bd>
2020-05-31 02:10:54 +0600

    Promises part 2

--- Angular/conFusion/src/app/app.module.ts
+++ Angular/conFusion/src/app/app.module.ts
@@ -19,6 +19,7 @@ import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms'; 
 import { MatSelectModule } from '@angular/material/select';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 import { AppComponent } from './app.component';
 
@@ -66,6 +67,7 @@ import { LoginComponent } from './login/login.component';
     MatDialogModule,
     MatSelectModule,
     MatSlideToggleModule,
+    MatProgressSpinnerModule,
     AppRoutingModule,
     FormsModule, 
     ReactiveFormsModule
